8d211a42d6db07da0f220f42f45dc433b089f641
unknown <galor_nimrod@hotmail.com>
2024-08-31 21:16:06 +0300

    unite script files

--- public/scripts/lista.js
+++ public/scripts/lista.js
@@ -164,7 +164,7 @@ function createDomList(parentDom, currentList){
                 createDomList(itemInner, currentItemObj)
             break
         }
-        // itemWrapperDom.setAttribute("tabIndex", "1")
+
         itemWrapperDom.tabIndex = 1
         listDom.appendChild(itemWrapperDom)
         parentDom.appendChild(listDom)
@@ -535,6 +535,153 @@ function userLangDirChange(dir){
     }
 }
 
+async function fetchData(action, method, dataToSend){
+    let accessToken = localStorage.getItem('accessToken')  // Get current access token
+
+    if(!accessToken){
+        accessToken = await refreshAccessToken()
+    }
+
+    const response = await fetch(action, { 
+            method,
+            body: JSON.stringify(dataToSend),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            }
+        })
+
+    if (response.status === 401) {  // Token expired or invalid
+        // Attempt to refresh token
+        const refreshResponse = await fetch('/api/refresh-token', {
+            method: 'POST',
+            credentials: 'include'  // Send cookies for refresh token
+        });
+
+        if (refreshResponse.ok) {
+            const data = await refreshResponse.json()
+            localStorage.setItem('accessToken', data.accessToken)  // Store new access token
+            return fetchData(action, method, dataToSend)  // Retry the request with new token
+        } else {
+            console.error('Failed to refresh token');
+        }
+    }else if(!response.ok){
+        topAlert('warning', response.status, response.statusText)
+    }else{
+        const data = await response.json();
+        return data;
+    }
+}
+
+async function refreshAccessToken(){
+    // Attempt to refresh token
+    const refreshResponse = await fetch('/api/refresh-token', {
+        method: 'POST',
+        credentials: 'include'  // Send cookies for refresh token
+    });
+
+    if (refreshResponse.ok) {
+        const data = await refreshResponse.json();
+        localStorage.setItem('accessToken', data.accessToken);  // Store new access token
+        return data.accessToken
+    } else {
+        console.error('Failed to refresh token');
+    }
+}
+
+
+function validateForm(event){
+    const form = event.currentTarget
+    form.classList.add('was-validated')
+
+    if (!form.checkValidity()) {// validation Failed
+        event.preventDefault()
+        event.stopPropagation()
+        return false
+    }
+
+    // start submit button spiner
+    form.classList.add('disabled')
+    // disabel submit buttn
+    event.submitter.disabled = true
+    
+    return true
+}
+
+// const shadowTextArea = document.createElement('textarea');
+
+// // Function to decode HTML entities
+// function decodeHTML(str) {
+//     shadowTextArea.innerHTML = str;
+//     return shadowTextArea.value;
+// }
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date()
+    d.setTime(d.getTime() + (exdays*24*60*60*1000))
+    let expires = "expires="+ d.toUTCString()
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function toggleClass(objId, className){
+    document.getElementById(objId).classList.toggle(className)
+    event.currentTarget.classList.toggle(className)
+}
+
+function topAlert(type, title, body){
+    document.getElementById('top-alert').classList.add('open')
+    document.getElementById('top-alert').querySelector('.alert').className = `alert alert-${type}`
+    document.getElementById('top-alert-title').innerHTML = title
+    document.getElementById('top-alert-body').innerHTML = body
+}
+
+function confirmPasswords(){
+    // Check if password and confirm password match
+    var password = document.getElementById('signup-password').value;
+    var confirmPassword = document.getElementById('confirmPassword')
+
+    if (password !== confirmPassword.value) {
+        event.preventDefault()
+        event.stopPropagation()
+
+        // Show custom validation message
+        confirmPassword.setCustomValidity('Passwords do not match')
+        confirmPassword.classList.add('is-invalid')
+
+    } else {
+        confirmPassword.setCustomValidity('')
+        confirmPassword.classList.remove('is-invalid')
+        confirmPassword.classList.add('is-valid')
+    }
+}
+
+// function profileDropdownClick(){
+//     event.stopPropagation()
+//     let obj = document.getElementById("profileDropdown")
+//     if(obj.classList.contains("show")){
+//         obj.classList.remove("show")
+//         document.body.removeEventListener('click', profileDropdownClick)
+//     }else{
+//         obj.classList.add("show")
+//         document.body.addEventListener('click', profileDropdownClick)
+//     }
+// }
 
 Array.prototype.move = function (from, to) {
     this.splice(to, 0, this.splice(from, 1)[0]);
