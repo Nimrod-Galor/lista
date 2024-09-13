d18b37be6d55ae12bb1369220ab5016581285079
unknown <galor_nimrod@hotmail.com>
2024-09-13 21:04:36 +0300

    extend rate limit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set('trust proxy', 1) /* number of proxies between user and server */
 
 const limiter = RateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
-    max: 20,
+    max: 60,
 })
 // Apply rate limiter to all requests
 app.use(limiter)
